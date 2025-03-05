6269149174f6a5bfad1502a71c89d9e3fb336d19
ruderude <rude1979@gmail.com>
2025-03-06 01:16:55 +0900

    値段を修正

--- src/components/modules/contents/Menu.tsx
+++ src/components/modules/contents/Menu.tsx
@@ -34,21 +34,21 @@ export const Menu = () => {
             ※喫煙可
           </div>
           <div className={styles.text_price}>
-            テーブルチャージ：<span className={styles.big_text}>1,000円</span> <br />
+            テーブルチャージ：<span className={styles.big_text}>500円</span> <br />
             １時間：<span className={styles.big_text}>1,500円</span>
           </div><br />
 
-          　入店時に発生するテーブルチャージが<b>1,000円</b>。<br />
+          　入店時に発生するテーブルチャージが<b>500円</b>。<br />
           その後1時間ごとに<b>1,500円</b>が追加されます。<br /><br />
 
-          例）入店し、1時間以内に退店した場合、2,500円のお会計となります。<br />
-          例）2時間滞在した場合、4,000円のお会計となります。<br /><br />
+          例）入店し、1時間以内に退店した場合、2,000円のお会計となります。<br />
+          例）2時間滞在した場合、3,500円のお会計となります。<br /><br />
 
           飲み物は、飲み放題（一部追い金アリ）<br /><br />
 
           カラオケは歌い放題となります。<br /><br />
 
-          ・店舗のLINE公式を追加いただく事でお通しにスナックなど乾き物がでます。<br />
+          ・店舗のLINE公式を追加いただく事でお通しにスナックなど乾き物がでます（おかわり自由）<br />
           ・基本的にフードメニューはありません。<br />
           ・食べ物の持ち込み可能です。（持ち込み料無料です）<br /><br />
 
@@ -57,16 +57,19 @@ export const Menu = () => {
           ※割もの代・ソフトドリンクは飲み放題に入っています。<br /><br />
 
           <span className={styles.drink_title}>▼焼酎</span><br />
-          鏡月、黒霧島、などが飲み放題です。<br /><br />
+          鏡月、いいちこ、黒霧島などが飲み放題です。<br /><br />
 
           <span className={styles.drink_title}>▼ビール</span><br />
           瓶ビールが飲み放題となっております。<br /><br />
 
-          <span className={styles.drink_title}>▼ウイスキー・バーボン・スコッチ</span><br />
-          角、ジャックダニエルが飲み放題になります。<br /><br />
+          <span className={styles.drink_title}>▼ウイスキー・バーボン</span><br />
+          角、ジャックダニエル、ジムビームが飲み放題になります。<br /><br />
 
-          <span className={styles.drink_title}>▼テキーラ</span><br />
-          1杯あたり1000円となります。（クエルボ・1800など）<br /><br />
+          <span className={styles.drink_title}>▼テキーラ（別料金）</span><br />
+          イエガー・マイスター　500円<br />
+          クエルボ・シルバー　500円<br />
+          クエルボ・ゴールド　500円<br />
+          クエルボ1800　1,000円br<br /><br />
 
           <span className={styles.drink_title}>▼その他カクテル</span><br />
           店内にあるリキュールからお好みのカクテルを注文できます。<br />
